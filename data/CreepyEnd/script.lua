91285d11f920b5af51c435a49e64b72720d137ec
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2024-06-22 13:35:59 -0700

    don't need to make another camera for this

--- data/CreepyEnd/script.lua
+++ data/CreepyEnd/script.lua
@@ -1,17 +1,7 @@
 function create()
 	luaDebugMode = true;
 	
-	runHaxeCode([[
-		import flixel.FlxCamera;
-		import psychlua.LuaUtils;
-		
-		var scareCam = FlxG.cameras.add(new FlxCamera(0, 0, 1280, 720), false);
-		scareCam.bgColor = 0xFF000000;
-		setVar('scareCam', scareCam);
-	]]);
-	
 	makeLuaSprite('creepy', 'gameAssets/Jumpscares/yellow');
-	setCam('creepy', 'scareCam');
 	addLuaSprite('creepy');
 	
 	doSound('xScream2', 1, 'yellowScream');
